4451f15a0a9a685da322217e9b8db45d33d95530
Mauricio-Nettoo <mauricio.almeida.dev@gmail.com>
2025-08-14 07:37:28 -0300

    feat: adds findFileInFolders to separate getAppExecutablePath responsabilities

--- main.ts
+++ main.ts
@@ -13,6 +13,27 @@ function getHomeDir(): string {
   return home;
 }
 
+async function findFileInFolders(
+  filePrefix: string,
+  baseDir: string,
+  folders: string[],
+): Promise<string | null> {
+  for (const folder of folders) {
+    const dir: string = path.join(baseDir, folder);
+    try {
+      for await (const entry of Deno.readDir(dir)) {
+        if (entry.isFile && entry.name.startsWith(filePrefix)) {
+          return path.join(dir, entry.name);
+        }
+      }
+    } catch (err) {
+      if (err instanceof Deno.errors.NotFound) continue;
+      throw err;
+    }
+  }
+  return null;
+}
+
 async function getAppExecutablePath(): Promise<string | null> {
   const placesToLookFor: string[] = [
     "Documents",
